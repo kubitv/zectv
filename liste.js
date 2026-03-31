0f94c94ecc8f22e7afd36d8433e82019d5ee3c48
kubitv <63615296+kubitv@users.noreply.github.com>
2026-03-31 11:51:07 +0300

    Add raw M3U playlist to liste.js

---
+++ liste.js
@@ -0,0 +1,8 @@
+// Baştaki ve sondaki ters tırnaklar ( ` ) çok önemli!
+const rawM3U = ` #EXTM3U
+#EXTINF:-1, Kanal 1
+http://puhtvhd.shop:8080/live/sinansert120/ymZWf3GIGE/98925.m3u8
+#EXTINF:-1, Kanal 2
+http://puhtvhd.shop:8080/live/sinansert120/ymZWf3GIGE/35762.m3u8
+... BURAYA O 13 MİLYON KARAKTERİN TAMAMINI YAPIŞTIR ...
+`;
